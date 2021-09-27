3f6fc59dcce3a6bc52454e78abc87beaf81c5cae
Albert <albert.dm90@gmail.com>
2021-09-27 10:46:48 -0400

    Back

--- V1/routes/api.php
+++ V1/routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', 'API\AuthController@register');
+Route::post('/login', 'API\AuthController@login');
+
+Route::group(['middleware' => 'auth:api', 'prefix' => 'auth'], function() {
+
+    Route::get('/user', 'API\AuthController@user');
+    Route::get('/logout', 'API\Auth\AuthController@logout');
+    
+});
